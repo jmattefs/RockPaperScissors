295610c58a20c917776311fca36bc80ae848d43a
jmattefs <josephmattefs@gmail.com>
2016-01-21 16:26:42 -0600

    while loop

--- RockPaperScissors/CheckWinner.cs
+++ RockPaperScissors/CheckWinner.cs
@@ -11,24 +11,27 @@ namespace RockPaperScissors
     public class CheckWinner
     {
 
-        public void checkWinnerAi(string humanChoice, string computerChoice)
+        public bool checkWinnerAi(string humanChoice, string computerChoice)
         {
             string human = StartGame.playerOneName;
 
             if (((humanChoice == "rock") && (computerChoice == "scissors")) || ((humanChoice == "paper") && (computerChoice == "rock")) || ((humanChoice == "scissors") && (computerChoice == "paper")) || ((humanChoice == "rock") && (computerChoice == "lizard")) || ((humanChoice == "paper") && (computerChoice == "Spock")) || ((humanChoice == "scissors") && (computerChoice == "lizard")) || ((humanChoice == "lizard") && (computerChoice == "Spock")) || ((humanChoice == "lizard") && (computerChoice == "paper")) || ((humanChoice == "Spock") && (computerChoice == "scissors")) || ((humanChoice == "Spock") && (computerChoice == "rock")))
             {
                 Console.WriteLine(human + " wins.");
+                return true;
             }
             else if (((humanChoice == "rock") && (computerChoice == "paper")) || ((humanChoice == "paper") && (computerChoice == "scissors")) || ((humanChoice == "scissors") && (computerChoice == "rock")) || ((humanChoice == "lizard") && (computerChoice == "rock")) || ((humanChoice == "Spock") && (computerChoice == "paper")) || ((humanChoice == "lizard") && (computerChoice == "scissors")) || ((humanChoice == "Spock") && (computerChoice == "lizard")) || ((humanChoice == "paper") && (computerChoice == "lizard")) || ((humanChoice == "scissors") && (computerChoice == "Spock")) || ((humanChoice == "rock") && (computerChoice == "Spock")))
             {
                 Console.WriteLine("The computer wins.");
+                return true;
             }
-            else if (humanChoice == computerChoice)
+            else 
             {
                 Console.WriteLine("Tie game. Reshoot.");
+                return false;
             }
         }
-        public void checkWinnerH2H(string humanChoice, string humanTwoChoice)
+        public bool checkWinnerH2H(string humanChoice, string humanTwoChoice)
         {
             string human = StartGame.playerOneName;
             string humanTwo = StartGame.playerTwoName;
@@ -36,14 +39,17 @@ namespace RockPaperScissors
             if (((humanChoice == "rock") && (humanTwoChoice == "scissors")) || ((humanChoice == "paper") && (humanTwoChoice == "rock")) || ((humanChoice == "scissors") && (humanTwoChoice == "paper")) || ((humanChoice == "rock") && (humanTwoChoice == "lizard")) || ((humanChoice == "paper") && (humanTwoChoice == "Spock")) || ((humanChoice == "scissors") && (humanTwoChoice == "lizard")) || ((humanChoice == "lizard") && (humanTwoChoice == "Spock")) || ((humanChoice == "lizard") && (humanTwoChoice == "paper")) || ((humanChoice == "Spock") && (humanTwoChoice == "scissors")) || ((humanChoice == "Spock") && (humanTwoChoice == "rock"))) 
             {
                 Console.WriteLine(human + " wins.");
+                return true;
             }
             else if (((humanChoice == "rock") && (humanTwoChoice == "paper")) || ((humanChoice == "paper") && (humanTwoChoice == "scissors")) || ((humanChoice == "scissors") && (humanTwoChoice == "rock")) || ((humanChoice == "lizard") && (humanTwoChoice =="rock")) || ((humanChoice == "Spock") && (humanTwoChoice=="paper")) || ((humanChoice == "lizard") && (humanTwoChoice== "scissors")) || ((humanChoice == "Spock") && (humanTwoChoice=="lizard")) || ((humanChoice == "paper") && (humanTwoChoice=="lizard")) || ((humanChoice == "scissors") && (humanTwoChoice== "Spock")) || ((humanChoice == "rock") && (humanTwoChoice == "Spock")))
             {
                 Console.WriteLine(humanTwo + " wins.");
+                return true;
             }
-            else if (humanChoice == humanTwoChoice)
+            else
             {
                 Console.WriteLine("Tie game. Reshoot.");
+                return false;
                 
             }
         }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -12,22 +12,28 @@ namespace RockPaperScissors
         {
             Game game = new Game();
             string type = game.startGame.start();
-            if(type == "1")
-            {
-                string humanResult = game.humanInput.humanChoice();
-                string aiResult = game.aiInput.getCompChoice();
-                game.checkWinner.checkWinnerAi(humanResult, aiResult);
-            } else if (type == "2")
-            {
-                string humanResult = game.humanInput.humanChoice();
-                string humanResultTwo = game.humanInputTwo.humanChoiceTwo();
-                game.checkWinner.checkWinnerAi(humanResult, humanResultTwo);
+            bool tie  = false;
 
-            } else if (type == "3")
+            while (!tie)
             {
-                game.startGame.start();
+                if (type == "1")
+                {
+                    string humanResult = game.humanInput.humanChoice();
+                    string aiResult = game.aiInput.getCompChoice();
+                    tie = game.checkWinner.checkWinnerAi(humanResult, aiResult);
+                }
+                else if (type == "2")
+                {
+                    string humanResult = game.humanInput.humanChoice();
+                    string humanResultTwo = game.humanInputTwo.humanChoiceTwo();
+                    tie = game.checkWinner.checkWinnerH2H(humanResult, humanResultTwo);
+
+                }
+                else if (type == "3")
+                {
+                    game.startGame.start();
+                }
             }
-
             
 
 
